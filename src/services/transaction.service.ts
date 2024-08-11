4637df8ba433f5eb6f65e9c56a14f5074e243fee
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-08-11 20:07:44 +0800

    feat: detail listing information

---
+++ src/services/transaction.service.ts
@@ -0,0 +1,15 @@
+import build from "next/dist/build";
+import { apiSlice } from "./base-query";
+
+export const transactionApi = apiSlice.injectEndpoints({endpoints: (build) => ({
+        checkAvailability: build.mutation({
+            query: (payload) => ({
+                url: "/transaction/is-available",
+                method: "POST",
+                body: payload,
+            }),
+        }),
+    }),
+});
+
+export const { useCheckAvailabilityMutation } = transactionApi;
