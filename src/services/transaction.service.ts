804b1eec9d8a30dd6cf0476f6e54fe51679bacf0
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-08-12 09:30:41 +0800

    feat: booking success

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -16,7 +16,13 @@ export const transactionApi = apiSlice.injectEndpoints({
         body: payload,
       }),
     }),
+    getTransactions: build.query({
+      query: (id) => ({
+        url: `/transaction/${id}`,
+        method: "GET",
+      }),
+    }),
   }),
 });
 
-export const { useCheckAvailabilityMutation, useTransactionMutation } = transactionApi;
+export const { useCheckAvailabilityMutation, useTransactionMutation, useGetTransactionsQuery } = transactionApi;
